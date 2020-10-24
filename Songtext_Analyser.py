5d30b9f5c70a992c461806e9e4775bec8d32a86c
Chris-Land <60221368+Chris-Land@users.noreply.github.com>
2020-10-24 15:21:20 +0200

    Update Songtext_Analyser.py

--- Songtext_Analyser.py
+++ Songtext_Analyser.py
@@ -1,34 +1,47 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter import Text
 
-def text_auslesen (URL):
+
+def open_file():
+    datei = filedialog.askopenfilename(initialdir="/", title="select File", filetypes=(("text", ".txt"), ("PDF", ".pdf"))) #returns filepath
+    pass 
+
+def test_URL():
+    test=str(input.get())
+    if (test[0:8]=="https://"):
+        print("ok")
+    else:
+        master.quit
+
+def open_URL (URL):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'lxml')
     title = soup.find(id="lyrics").get_text()
     wortliste(title)
 
-def wortliste (Text):
+def wordlist (Text):
     pass
 
-def Graphausgabe():
+def graph():
     pass
 
-def tkinter_starteingabe():
+    
+
+if __name__ == "__main__":
     URL = ""
     frage = "Bitte URL der Liedtextseite eingeben:"
     master = tk.Tk()
-    canvas = tk.Canvas(master, height=700, width=700, bg="green")
-    canvas.pack( )
-    frame = tk.Frame(master, bg="white") 
-    frame.place(relwidth = 0.8, relheigh = 0.8,relx = 0.1, rely = 0.1 )
-    
-    #entry.grid()
-    
+    Label(master, text="URL").grid(row=0)
+    input = Entry(master)
+    input.grid(row=0, column=1)
+    Button(master, text='Open File', command=open_file).grid(row=3, column=0, sticky=W, pady=4)
+    Button(master, text='Open URL', command=test_URL).grid(row=3, column=1, sticky=W, pady=4)
     #text_auslesen(URL)
     #text_auslesen("https://www.songtexte.com/songtext/manowar/black-wind-fire-and-steel-3bdf508c.html")
     master.mainloop()
-
-if __name__ == "__main__":
-    tkinter_starteingabe()
     
